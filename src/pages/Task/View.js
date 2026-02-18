25eb4ddc0aec76c28156a13f71ebab3d7eb154e5
Gabriel Brito <gabriel@mail.com>
2026-02-18 18:08:33 -0300

    Refactor TaskView to improve button state handling and input monitoring
    Updated the TaskView class to enhance the logic for the "Clear All Tasks" button by directly using the length of tasks to determine its disabled state. Introduced a new private method, #watchDisabledFormSubmitButton, to streamline the monitoring of input changes and manage the disabled state of the submit button. These changes improve code clarity and maintainability while ensuring a better user experience.

--- src/pages/Task/View.js
+++ src/pages/Task/View.js
@@ -150,9 +150,9 @@ export default class TaskView {
       `;
   }
 
-  #templateClearAllTasksButton(disabledClearAllTasksButton) {
+  #templateClearAllTasksButton(tasksLength) {
     return `
-      <button data-js="task-clear" class="${styles.buttonClear} ${disabledClearAllTasksButton ? "" : styles.buttonClearDisabled}" ${disabledClearAllTasksButton ? "" : "disabled"} type="button">
+      <button data-js="task-clear" class="${styles.buttonClear} ${tasksLength === 0 ? styles.buttonClearDisabled : ""}" ${tasksLength === 0 ? "disabled" : ""} type="button">
           <span class="${styles.buttonClearText}">Limpar tudo</span>
           <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
             <path stroke-linecap="round" stroke-linejoin="round" d="M14.74 9l-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 0 1-2.244 2.077H8.084a2.25 2.25 0 0 1-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 0 0-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 0 1 3.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 0 0-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 0 0-7.5 0" />
@@ -208,15 +208,26 @@ export default class TaskView {
     `;
   }
 
-  render(domainState, editingTask = null, filter = "all", search = "") {
-    const disabledClearAllTasksButton = domainState.tasks.length > 0;
+  #watchDisabledFormSubmitButton() {
+    this.$root.off("input.taskflow", "[data-js='task-input']");
+    this.$root.on("input.taskflow", "[data-js='task-input']", (e) => {
+      const value = String($(e.currentTarget).val() ?? "").trim();
+      const disabled = value.length === 0;
+  
+      const $form = $(e.currentTarget).closest("[data-js='task-form']");
+      $form.find("[data-js='task-submit']")
+        .toggleClass(styles.buttonDisabled, disabled)
+        .prop("disabled", disabled);
+    });
+  }
 
+  render(domainState, editingTask = null, filter = "all", search = "") {
     this.$root.html(
       `
         <div class="${styles.container}">
           ${this.#templateHeader(domainState.stats)}
           ${this.#templateForm(editingTask)}
-            ${this.#templateClearAllTasksButton(disabledClearAllTasksButton)}
+            ${this.#templateClearAllTasksButton(domainState.tasks.length)}
             <br />
             ${this.#templateToolbar(filter, search)}
             ${this.#templateTaskList(domainState.tasks, filter)}
@@ -224,22 +235,7 @@ export default class TaskView {
       `,
     );
 
-    const form = this.$root.find("[data-js='task-form']");
-    const input = this.$root.find("[data-js='task-input']");
-
-    if (!!editingTask) {
-      input.val(editingTask.title).data("id", editingTask.id);
-    }
-
-    // watch input changes
-    input.off("input.taskflow");
-    input.on("input.taskflow", () => {
-      const isButtonSubmitDisabled = !input.val()?.trim();
-      form
-        .find("[data-js='task-submit']")
-        .toggleClass(styles.buttonDisabled, isButtonSubmitDisabled)
-        .prop("disabled", isButtonSubmitDisabled);
-    });
+    this.#watchDisabledFormSubmitButton();
   }
 
   filterChange(handler) {
