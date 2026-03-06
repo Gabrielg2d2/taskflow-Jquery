eccbe2b5bce5779407cbbbeb8da578293437a885
Gabriel Brito <gabriel@mail.com>
2026-03-06 10:23:47 -0300

    feat: enhance InputBase stories with additional states and props
    Updated the InputBase component stories in Storybook to include new variations for disabled, error, label, and placeholder states. Improved type safety by specifying InputBaseProps in the render function and story definitions, ensuring better documentation and usability of the component.

--- packages/design-system/src/components/Inputs/InputBase/index.stories.ts
+++ packages/design-system/src/components/Inputs/InputBase/index.stories.ts
@@ -1,18 +1,44 @@
 import type { Meta, StoryObj } from "@storybook/html";
-import { InputBase } from ".";
+import { InputBase, type InputBaseProps } from ".";
 
-const meta: Meta = {
+const meta = {
   title: "Components/Inputs/InputBase",
   tags: ["autodocs"],
-  render: () => InputBase({
+  render: (args: InputBaseProps) => InputBase(args),
+  args: {
     label: "Input Base",
     placeholder: "Digite aqui...",
     type: "text"
-  }),
-};
+  }
+} satisfies Meta<InputBaseProps>;
 
 export default meta;
 
-type Story = StoryObj<typeof meta>;
+type Story = StoryObj<InputBaseProps>;
 
 export const Default: Story = {};
+
+
+export const Disabled: Story = {
+  args: {
+    disabled: true
+  }
+};
+
+export const Error: Story = {
+  args: {
+    error: "Este é um erro"
+  }
+};
+
+export const Label: Story = {
+  args: {
+    label: "Input Base Label"
+  }
+};
+
+export const Placeholder: Story = {
+  args: {
+    placeholder: "Digite aqui... Placeholder"
+  }
+};
