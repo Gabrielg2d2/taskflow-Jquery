bc4d262ae9dacaeaa6b036bfe38ec42e13ca5671
Gabriel Brito <gabriel@mail.com>
2026-03-08 15:37:45 -0300

    feat: restructure input styles and remove deprecated utilities
    Introduced a new input.css file to encapsulate the styles for the ds-input component, enhancing modularity and maintainability. Removed the outdated utilities index file and updated the Storybook preview to include the new components directory. This change streamlines the styling approach and improves the organization of the design system.

--- packages/ds/styles/.storybook/preview.ts
+++ packages/ds/styles/.storybook/preview.ts
@@ -1,6 +1,7 @@
 import type { Preview } from "@storybook/html";
 import "../src/styles/tokens.css";
 import "../src/styles/globals.css";
+import "../src/styles/components";
 import { initArbitraryStyles } from "../src/utils/arbitrary-styles";
 
 initArbitraryStyles();
