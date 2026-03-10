3fe6feda488e71d9f1e0377f5c27200b2c049e97
Gabrielg2d2 <gabriel.g2d2@gmail.com>
2026-03-10 19:48:30 -0300

    feat: add Storybook stories for button and input components
    Introduced new Storybook stories for the ButtonBase and InputBase components, enhancing the documentation and testing capabilities of the design system. Updated package.json to include a new development script for design system styles, and added corresponding CSS for button styling to improve component presentation.

---
+++ packages/ds/styles/src/stories/button.stories.ts
@@ -0,0 +1,41 @@
+import type { Meta, StoryObj } from "@storybook/html";
+
+const meta = {
+  title: "Components/Buttons/ButtonBase",
+  render: (args) => `
+    <div data-theme="light">
+      <button class="ds-button ds-button:${args.variant} ds-button:${args.type} ${args.disabled ? "disabled" : ""}">
+        ${args.text}
+      </button>
+    </div>
+  `,
+  args: {
+    variant: "primary",
+    type: "button",
+    disabled: false,
+    text: "Text button",
+  },
+  argTypes: {
+    variant: {
+      control: "select",
+      options: ["primary", "secondary", "success", "warning", "info", "error"],
+    },
+    type: {
+      control: "select",
+      options: ["button", "submit", "reset"],
+    },
+    disabled: {
+      control: "boolean",
+    },
+    text: {
+      control: "text",
+    },
+  },
+} satisfies Meta;
+
+export default meta;
+
+export const Base: StoryObj = {
+//   args: {
+//   },
+};
