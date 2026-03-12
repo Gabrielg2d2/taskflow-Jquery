56adff85761aeb1a4d87b6d112b9127faf4a9937
Gabriel Brito <gabriel@mail.com>
2026-03-12 08:48:10 -0300

    refactor: enhance button story with additional variants and size support
    Updated the button story to include multiple variants (primary, secondary, success, warning, info, error) and added size support. This enhancement improves the demonstration of the button's visual states and usability in Storybook, providing a more comprehensive overview of the component's capabilities.

--- packages/ds/styles/src/stories/button.stories.ts
+++ packages/ds/styles/src/stories/button.stories.ts
@@ -4,7 +4,15 @@ const meta = {
   title: "Components/Buttons/ButtonBase",
   render: (args) => `
     <div data-theme="light">
-      <button class="ds-button ds-button:${args.variant} ds-button:${args.type} ${args.disabled ? "disabled" : ""}">
+      <button
+        class="ds-button
+          ds-button:${args.variant}
+          ds-button:${args.type}
+          ${args.disabled ? "disabled" : ""}
+          ${args.size ? "ds-button:${args.size}" : ""}
+        "
+         ${args.disabled ? "disabled" : ""}
+        >
         ${args.text}
       </button>
     </div>
@@ -37,6 +45,90 @@ export default meta;
 
 export const Base: StoryObj = {
   args: {
-    variant: "error"
+    variant: "primary",
+    disabled: true
   }
+};
+
+export const AllVariants: StoryObj = {
+  render: (args) => `
+    <div data-theme="light" class="flex-col gap-lg p-lg justify-center items-center">
+      <button
+        class="ds-button
+          ds-button:primary
+          ds-button:${args.type}
+          ${args.disabled ? "disabled" : ""}
+          ${args.size ? "ds-button:${args.size}" : ""}
+        "
+         ${args.disabled ? "disabled" : ""}
+        >
+        ${args.text}
+      </button>
+
+      <button
+        class="ds-button
+          ds-button:secondary
+          ds-button:${args.type}
+          ${args.disabled ? "disabled" : ""}
+          ${args.size ? "ds-button:${args.size}" : ""}
+        "
+         ${args.disabled ? "disabled" : ""}
+        >
+        ${args.text}
+      </button>
+
+      <button
+        class="ds-button
+          ds-button:success
+          ds-button:${args.type}
+          ${args.disabled ? "disabled" : ""}
+          ${args.size ? "ds-button:${args.size}" : ""}
+        "
+         ${args.disabled ? "disabled" : ""}
+        >
+        ${args.text}
+      </button>
+
+      <button
+        class="ds-button
+          ds-button:warning
+          ds-button:${args.type}
+          ${args.disabled ? "disabled" : ""}
+          ${args.size ? "ds-button:${args.size}" : ""}
+        "
+         ${args.disabled ? "disabled" : ""}
+        >
+        ${args.text}
+      </button>
+
+      <button
+        class="ds-button
+          ds-button:info
+          ds-button:${args.type}
+          ${args.disabled ? "disabled" : ""}
+          ${args.size ? "ds-button:${args.size}" : ""}
+        "
+         ${args.disabled ? "disabled" : ""}
+        >
+        ${args.text}
+      </button>
+
+      <button
+        class="ds-button
+          ds-button:error
+          ds-button:${args.type}
+          ${args.disabled ? "disabled" : ""}
+          ${args.size ? "ds-button:${args.size}" : ""}
+        "
+         ${args.disabled ? "disabled" : ""}
+        >
+        ${args.text}
+      </button>
+    </div>
+  `,
+  args: {
+    type: "button",
+    disabled: false,
+    text: "Text button",
+  },
 };
